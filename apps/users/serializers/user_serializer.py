b33955ef678807f6527aee9a365d7672530d6536
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-03-11 21:35:03 +0545

    Applying owner permissions to user-detail and different json response for owner and public

--- apps/users/serializers/user_serializer.py
+++ apps/users/serializers/user_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from users.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class OwnerUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
@@ -11,6 +11,23 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name', 
             'last_name',
             'email',
+            'bio',
+            'profile_picture',
+        ]
+        extra_kwargs = {
+            'username': {'read_only': True},
+            'profile_picture': {'required': False, 'allow_null': True},
+        }
+
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name', 
+            'last_name',
             'bio',
             'profile_picture',
         ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers.register_serializer import UserRegisterSerializer
-from users.serializers.user_serializer import UserSerializer
+from users.serializers.user_serializer import OwnerUserSerializer, PublicUserSerializer
+from users.permissions import IsOwnerOrReadOnly
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -12,8 +13,14 @@ class UserRegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
 
 
-class UserDetailView(generics.RetrieveAPIView):
+class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = PublicUserSerializer
     lookup_field = 'username'
-    permission_classes = [AllowAny]
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_serializer_class(self):
+        user = self.request.user
+        if user.is_authenticated and (user.username == self.kwargs.get('username')):
+            return OwnerUserSerializer
+        return super().get_serializer_class()
