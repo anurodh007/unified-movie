d8d259db04fd236770aaded1f65bd9622c31037d
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-03-12 16:26:20 +0545

    Creating Genre Serializer

---
+++ apps/movies/serializers/genre_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from movies.models import Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = [
+            'id',
+            'tmdb_id',
+            'name',
+        ]
