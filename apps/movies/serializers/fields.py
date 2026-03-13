5b29304524ac9add4c92b234c9b122ad3352838c
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-03-13 16:13:42 +0545

    Mapping custom TMDBImageField to standard OpenAPI type

--- apps/movies/serializers/fields.py
+++ apps/movies/serializers/fields.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
 from config.env import env
 
 
@@ -9,6 +10,7 @@ IMAGE_SIZE = 'w500'
 """
 Custom Field to build image path 
 """
+@extend_schema_field({'type': 'string', 'format': 'uri'})
 class TMDBImageField(serializers.Field):
     def to_representation(self, value):
         return f'{IMAGE_BASE_URL}/{IMAGE_SIZE}{value}' if value else ''
