96227f3a690be9d7909aed6393ab46654993dff9
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-03-13 17:32:16 +0545

    Filter by genres(multiple)

--- apps/movies/filters.py
+++ apps/movies/filters.py
@@ -6,10 +6,16 @@ class MovieFilter(django_filters.FilterSet):
     year = django_filters.NumberFilter(field_name='release_date', lookup_expr='year')
     year_range = django_filters.NumericRangeFilter(field_name='release_date', lookup_expr='year__range')
 
-    genre = django_filters.CharFilter(field_name='genres__name', lookup_expr='icontains')
+    genres = django_filters.CharFilter(method='filter_by_all_genres')
 
     class Meta:
         model = Movie
         fields = {
             'runtime': ['lt'],
         }
+
+    def filter_by_all_genres(self, queryset, name, value):
+        genres_list = [g.strip() for g in value.split(',') if g.strip()]
+        for genre in genres_list:
+            queryset = queryset.filter(genres__name__iexact=genre)
+        return queryset.distinct()
