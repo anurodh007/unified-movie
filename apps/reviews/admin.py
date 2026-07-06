65a512cad6aa0ff1b49626172cf08293d7ea0ddb
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-07-06 14:37:24 +0545

    Registering Review models into admin

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from reviews.models import Review, ReviewLike, ReviewComment
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(ReviewLike)
+class ReviewLikeAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(ReviewComment)
+class ReviewCommentAdmin(admin.ModelAdmin):
+    ...
