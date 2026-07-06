dd7aea77a96121ff9580dbb3fd1d8e94a714e202
anurodh007 <anurodh.upadhyaya17@gmail.com>
2026-07-06 14:48:04 +0545

    Creating and Registering Watchlist Model

--- apps/watchlist/admin.py
+++ apps/watchlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from watchlist.models import Watchlist
 
-# Register your models here.
+
+@admin.register(Watchlist)
+class WatchlistAdmin(admin.ModelAdmin):
+    ...
--- apps/watchlist/models.py
+++ apps/watchlist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from users.models import User
+from movies.models import Movie
 
-# Create your models here.
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watchlist')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='watchlisted_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=('user', 'movie'),
+                name='unique_user_movie_watchlist'
+            )
+        ]
